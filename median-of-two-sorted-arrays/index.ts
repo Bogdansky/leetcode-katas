23fc7205bbb63fad1ca490eda05d01021ffc8cb9
Bahdan Kalishok <bahdan_kalishok@epam.com>
2025-01-14 19:23:13 +0500

    Add kata

---
+++ median-of-two-sorted-arrays/index.ts
@@ -0,0 +1,38 @@
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    // merge two array
+    // get median
+    // 1. do it without merge
+    /// 1.1 calculate whether res array's length is even or odd
+    /// calculate median of 
+    /// 2.1. three cases: 
+    //// 2.1.1 first array's last el is less than first el of second one
+    //// 2.1.1 first array's first el is more than second el of second one
+    // 2. do it with merge and with fast sorting (easiest way because sort algo could be googled)
+    // 3. put all items in dictionary, 
+};
+
+type Dictionary = {
+    [key: number]: number | undefined
+}
+
+function medianWithDictionary(nums1: number[], nums2: number[]): number{
+    const dict: Dictionary = {};
+    let count = 0;
+    nums1.forEach(n => {
+        const exists = !dict[n];
+        if (!exists) return;
+        dict[n] = n;
+        count++;
+    });
+    nums2.forEach(n => {
+        const exists = !dict[n];
+        if (!exists) return;
+        dict[n] = n;
+        count++;
+    });
+    const isEven = count % 2 === 0;
+    let median = 0;
+    if (isEven) {
+        median = 
+    }
+}
